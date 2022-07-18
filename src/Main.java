7e0b7a11334e5c9c0312527dc5a56811484252a7
Appolia <VasyGrenkin@gmail.com>
2022-07-18 19:59:02 +0300

    ex2

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,18 @@ public class Main {
         } else if (clientOS == 0){
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
+
+        //ex2
+        int clientDeviceYear = 2016;
+        int OS = 1;
+        if (clientDeviceYear <= 2015 && OS == 1) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (clientDeviceYear <= 2015 && OS == 0) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientDeviceYear > 2015 && OS == 1) {
+            System.out.println("Установите новую версию приложения для Android по ссылке");
+        } else if (clientDeviceYear > 2015 && OS == 0) {
+            System.out.println("Установите новую версию приложения для iOS по ссылке");
+        }
     }
 }
